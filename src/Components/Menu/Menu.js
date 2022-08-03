9bd908ed62fc4bdb66a41c0cb1b898cb013441ad
Trish Fox-Collis <tfoxcollis@gmail.com>
2022-08-03 12:32:33 -0700

    build menu

---
+++ src/Components/Menu/Menu.js
@@ -0,0 +1,52 @@
+import React from 'react'
+import './Menu.css'
+
+const Menu = () => {
+  return (
+    <div className="menu-main">
+      <div>
+      <h2>Pick your zip</h2>
+      <h3>NorthWest</h3>
+      <p>89129<br></br>
+         89134<br></br>
+         89128<br></br>
+         89145<br></br>
+         89117<br></br>
+         89129<br></br>
+      </p>
+      <h3>NorthEast</h3>
+      <p>89101<br></br>
+         89106<br></br>
+         89032<br></br>
+         89031<br></br>
+         89115<br></br>
+         89156<br></br>
+         89110<br></br>
+         89081<br></br>
+      </p>
+      <h3>SouthEast</h3>
+      <p>89123<br></br>
+         89183<br></br>
+         89052<br></br>
+         89074<br></br>
+         89012<br></br>
+         89119<br></br>
+         89120<br></br>
+      </p>
+      <h3>SouthWest</h3>
+      <p>89179<br></br>
+         89141<br></br>
+         89178<br></br>
+         89139<br></br>
+         89148<br></br>
+         89113<br></br>
+         89147<br></br>
+      </p>
+
+      </div>
+
+    </div>
+  )
+}
+
+export default Menu
