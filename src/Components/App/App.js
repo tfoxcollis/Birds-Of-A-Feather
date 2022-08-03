aa2ffc60585d6689ac8e378ed344a3858ae0f160
Trish Fox-Collis <tfoxcollis@gmail.com>
2022-08-02 20:32:39 -0700

    Initial Re commit

---
+++ src/Components/App/App.js
@@ -0,0 +1,16 @@
+import 'src/Components/App/App.css';
+import Welcome from "../Welcome/Welcome"
+// import PropTypes from 'prop-types';
+
+import React from 'react'
+import { Route } from 'react-router';
+
+const App = () => {
+  return (
+    <Route>
+      <Welcome />
+    </Route>
+  )
+}
+
+export default App
---
+++ src/Components/Welcome/Welcome.js
@@ -0,0 +1,19 @@
+import "src/Components/Welcome/Welcome.css"
+import React from 'react'
+
+const Welcome = () => {
+  return (
+    <div className="welcome-container">
+      <h1 className="welcome-title">Birds of a Feather</h1>
+      <p>Welcome to Birds of a Feather! 
+       Our goal is to connect LGBTQ+ families in the Las Vegas area.
+       After you enter, select a zip code and see if any other families have set up events near you. 
+       See an event you want to attend, hit that RSVP button and let the fam know you'll be there! 
+       Because Birds of A Feather Flock Together
+       </p>
+
+    </div>
+  )
+}
+
+export default Welcome
