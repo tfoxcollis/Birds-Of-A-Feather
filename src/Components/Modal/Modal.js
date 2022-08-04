f6cac1b75937bc08d3f8172fffcd0c2fd45457c2
Trish Fox-Collis <tfoxcollis@gmail.com>
2022-08-04 00:04:29 -0700

    Add Map to Modal

--- src/Components/Modal/Modal.js
+++ src/Components/Modal/Modal.js
@@ -1,7 +1,19 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import "./Modal.css.scss"
 
 const Modal = ({showModal, modalEvent, toggleModal}) => {
+
+  useEffect(() => {
+    if(!!modalEvent) {
+      window.L.mapquest.key = process.env.REACT_APP_MAPQUEST_KEY;
+      window.L.mapquest.map('map', {
+        center: [modalEvent.location.lat, modalEvent.location.lng],
+        layers: window.L.mapquest.tileLayer('map'),
+        zoom: 50
+      });
+    }
+  })
+
   return (
     <div className="modal-container off" id="modal-container">
       <div className="modal off" id="modal">
@@ -9,6 +21,7 @@ const Modal = ({showModal, modalEvent, toggleModal}) => {
           <>
             <h2>Modal Window</h2>
             <div className="modal-content">{modalEvent.description}</div>
+            <div id="map" className="mapquest-map"></div>
             <div className="actions">
               <button onClick={(e) => toggleModal(e)}>Close</button>
             </div>
