b9f5f896619c50cfd50cfc3e16734359b3b090a4
Trish Fox-Collis <tfoxcollis@gmail.com>
2022-08-04 21:36:27 -0700

    fix http

--- src/Components/EventForm/EventForm.js
+++ src/Components/EventForm/EventForm.js
@@ -32,7 +32,7 @@ const EventForm = ({eventCards,setEventCards}) => {
 
   const fetchPoi = (e) => {
     e.preventDefault(e)
-    const url = `http://www.mapquestapi.com/search/v3/prediction?key=${process.env.REACT_APP_MAPQUEST_KEY}&q=${e.target["place-search-input"].value}&collection=address,adminArea,airport,category,franchise,poi&limit=5&location=-115.140597,36.169202`
+    const url = `https://www.mapquestapi.com/search/v3/prediction?key=${process.env.REACT_APP_MAPQUEST_KEY}&q=${e.target["place-search-input"].value}&collection=address,adminArea,airport,category,franchise,poi&limit=5&location=-115.140597,36.169202`
     fetch(url)
       .then(response => response.json())
       .then(data => {
