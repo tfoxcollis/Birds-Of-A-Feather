2fd351c965e107b753c858adb1f1e0817cf69753
Trish Fox-Collis <tfoxcollis@gmail.com>
2022-08-05 21:40:01 -0700

    add test to check cancel rsvp

--- cypress/e2e/rsvp.cy.js
+++ cypress/e2e/rsvp.cy.js
@@ -12,7 +12,6 @@ describe("app", () => {
 
   it("should be able to click home and select event and rsvp to it and have it show on the RSVP page", () => {
     cy.get(".nav-links").contains("Home").click()
-      .url().should("eq", "http://localhost:3000/home")
       .get(".menu-container").contains("North West").click()
       .get(".content").contains("89129").click()
       .get(".toggle-button").click()
@@ -20,5 +19,18 @@ describe("app", () => {
       .get(".nav-links").contains("RSVP").click()
       .get(".event-card").contains("h3", "Picnic in the park")
 
+  })
+
+  it("should be able to cancel rsvp after it has been added", () => {
+    cy.get(".nav-links").contains("Home").click()
+      .get(".menu-container").contains("North West").click()
+      .get(".content").contains("89129").click()
+      .get(".toggle-button").click()
+      .get(".rsvp-button").click()
+      .get(".nav-links").contains("RSVP").click()
+      .get(".event-card").contains("View Event").click()
+      .get(".cancel-rsvp-button").should("be.visible").click()
+      .get(".close-modal").click()
+      .get(".no-events-message").contains("You have not RSVP'd to any events! Go check some out!")
   })
 })
